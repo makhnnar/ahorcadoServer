ae816f178d2b0388ce1509fbb5ed79d295aa879d
Pedro Gomez <makhnnar@gmail.com>
2018-12-14 15:56:16 -0400

    distribuyendo la logica del juego

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var intentos=6;
+var letras=[];
+
+var palabras={
+  palabra1:'No hay palabra',
+  pista1:'No hay pista',
+  palabra2:'No hay palabra'
+  pista2:'No hay pista'
+};
+
 var PLAYERSTATE = ["sin jugar","esperando","jugando"];
 var contClientes = 0;
 var clientesOnline = [];
@@ -20,6 +30,7 @@ io.on('connection', function(socket){
   socket.estado = PLAYERSTATE[0];
   clientesOnline.push(socket);
 
+  
   emparejarJugadores(clientesOnline);
   
   socket.emit('enviarCliente',{data:'Bienvenido cliente '+contClientes});
@@ -36,26 +47,27 @@ io.on('connection', function(socket){
                 indicePartida=j;
             }
     }
+
     console.clear();
     
     if (partidas[indicePartida].apuesta1 !== undefined && partidas[indicePartida].apuesta1 !== 0) {
             //comparo
             if (partidas[indicePartida].apuesta1 > numeroAleatorio) {
                  var pos1=buscarPosicion(partidas[indicePartida].jugador1,clientesOnline);
-                 clientesOnline[pos1].prioridad=2;
+                 clientesOnline[pos1].prioridad = 2;
                  clientesOnline[pos1].estado=PLAYERSTATE[0];
                  var pos2=buscarPosicion(partidas[indicePartida].jugador2,clientesOnline);
-                 clientesOnline[pos2].prioridad=3;
-                 clientesOnline[pos2].estado=PLAYERSTATE[0];
+                 clientesOnline[pos2].prioridad = 3;
+                 clientesOnline[pos2].estado = PLAYERSTATE[0];
                  emparejarJugadores(clientesOnline);
                  emparejarJugadores(clientesOnline);
                  partidas.splice(indicePartida,1);                       
             }else if(numeroAleatorio > partidas[indicePartida].apuesta1){
                 var pos2=buscarPosicion(partidas[indicePartida].jugador2,clientesOnline);
-                clientesOnline[pos2].prioridad=2;
-                clientesOnline[pos2].estado=PLAYERSTATE[0];
+                clientesOnline[pos2].prioridad = 2;
+                clientesOnline[pos2].estado = PLAYERSTATE[0];
                 var pos1=buscarPosicion(partidas[indicePartida].jugador1,clientesOnline);
-                clientesOnline[pos1].prioridad=3;
+                clientesOnline[pos1].prioridad = 3;
                 clientesOnline[pos1].estado=PLAYERSTATE[0];
                 emparejarJugadores(clientesOnline);
                 emparejarJugadores(clientesOnline);
@@ -71,21 +83,21 @@ io.on('connection', function(socket){
           //comparo
             if (partidas[indicePartida].apuesta2 > numeroAleatorio) {
                 var pos2=buscarPosicion(partidas[indicePartida].jugador2,clientesOnline);
-                clientesOnline[pos2].prioridad=2;
-                clientesOnline[pos2].estado=PLAYERSTATE[0];
+                clientesOnline[pos2].prioridad = 2;
+                clientesOnline[pos2].estado = PLAYERSTATE[0];
                 var pos1=buscarPosicion(partidas[indicePartida].jugador1,clientesOnline);
-                clientesOnline[pos1].prioridad=3;
-                clientesOnline[pos1].estado=PLAYERSTATE[0];
+                clientesOnline[pos1].prioridad = 3;
+                clientesOnline[pos1].estado = PLAYERSTATE[0];
                 emparejarJugadores(clientesOnline);
                 emparejarJugadores(clientesOnline);
                 partidas.splice(indicePartida,1);
             }else if(numeroAleatorio > partidas[indicePartida].apuesta2){
                 var pos1=buscarPosicion(partidas[indicePartida].jugador1,clientesOnline);
-                clientesOnline[pos1].prioridad=2;
-                clientesOnline[pos1].estado=PLAYERSTATE[0];
+                clientesOnline[pos1].prioridad = 2;
+                clientesOnline[pos1].estado = PLAYERSTATE[0];
                 var pos2=buscarPosicion(partidas[indicePartida].jugador2,clientesOnline);
-                clientesOnline[pos2].prioridad=3;
-                clientesOnline[pos2].estado=PLAYERSTATE[0];
+                clientesOnline[pos2].prioridad = 3;
+                clientesOnline[pos2].estado = PLAYERSTATE[0];
                 emparejarJugadores(clientesOnline);
                 emparejarJugadores(clientesOnline);
                 partidas.splice(indicePartida,1);
@@ -110,11 +122,47 @@ io.on('connection', function(socket){
         }
         console.log('---------------------------------------------------');
   });
-
-  socket.on('enviarServer', function(msg){
-    io.emit('enviarCliente', {data:msg});
+  
+  socket.on('enviarDatos',function(msg){
+      var palabra = msg.palabra;
+      var pista = msg.pista;
+      if (palabras.palabra1 === 'No hay palabra') {
+          palabra.palabra1 = palabra;
+          palabra.pista1 = pista;
+      }else if(palabras.palabra2 === 'No hay palabra'){
+           palabra.palabra2 = palabra;
+           palabra.pista2 = pista;
+      }      
+      if (palabras.palabra1 !== 'No hay palabra' && palabras.palabra2 !== 'No hay palabra') {
+          posicion1=buscarPosicion(newPartida.jugador1,clientesOnline);
+          clientesOnline[posicion1].emit('recibirPalabra',{palabra:palabra.palabra2,pista:palabra.pista2});    
+          posicion2=buscarPosicion(newPartida.jugador2,clientesOnline);
+          clientesOnline[posicion2].emit('recibirPalabra',{palabra:palabra.palabra1,pista:palabra.pista1});
+          palabras.palabra1:'No hay palabra';
+          palabras.pista1:'No hay pista';
+          palabras.palabra2:'No hay palabra';
+          palabras.pista2:'No hay pista';
+      } 
   });
-    
+  
+  socket.on('enviarLetra',function(msg){
+    var letra=msg.letra;
+    var mostrar=msg.mostrar;
+    letras.push(letra);
+});
+
+  socket.on('resultado',function(msg){
+      palabra=msg.palabra;
+      var cadena='';
+
+      for (var q = 0; q < letras.length; q++) {
+          cadena=cadena+letras[q];
+      }
+
+      if (cadena===palabra) {
+          //Gano
+      }
+
 });
 
 http.listen(3000, function(){
@@ -153,7 +201,7 @@ var emparejarJugadores=function(clientes){
         clientes[posicion2].emit('jugar',{id:clientes[posicion2].id,numero:clientes[posicion2].numero});
         newPartida.jugador1 = 0;
         newPartida.jugador2 = 0;
-      }
+     }
   }
 };
 
@@ -177,6 +225,7 @@ var buscarJugadorYCambiarStado=function(clientes){
     console.log('Encontrado con prioridad: '+prioridad);
     return encontrado;
 };
+
 var buscarPosicion=function(numero,clientes){
     let posicion1;
       for (let x = 0; x < clientes.length; x++) {
