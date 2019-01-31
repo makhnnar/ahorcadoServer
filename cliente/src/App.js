ac4a7ed64c15a4bba82d68f34aa9d6d37befe871
Richard Vallenilla <richardjvallenilla@gmail.com>
2019-01-31 16:55:39 -0400

    React Navigator web

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import ButtonImage from './ButtonImage';
 import './App.css';
+import { createNavigator,
+  SwitchRouter,
+  SceneView } from "@react-navigation/core";
+import { createBrowserApp } from "@react-navigation/web";
+import Login from './views/login/Login.js'
+import Alone from './views/alone/Alone.js'
+import Forgot from './views/forgot/Forgot.js'
+import Signup from './views/signup/Signup.js'
+import Vs from './views/vs/Vs.js'
+import Wellcome from './views/wellcome/Wellcome.js'
+
+const MyNavigator = createSwitchNavigator(routes);
+
+const App = createBrowserApp(MyNavigator);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <ButtonImage/>
-          <ButtonImage/>
-        </header>
+      	<Login/>
       </div>
     );
   }
 }
 
+const AppNavigator = createNavigator(
+  SidebarView,
+  SwitchRouter({
+    Login,
+    Alone,
+    Forgot,
+    Signup,
+    Vs,
+    Wellcome
+  }),
+  {}
+);
+
+const App = createBrowserApp(AppNavigator);
 export default App;
